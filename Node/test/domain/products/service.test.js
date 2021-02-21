3e5b609e5387fbede138baca38a32686f1d5e722
juablaz <jc-blazquez@hotmail.com>
2021-02-21 18:57:52 +0100

    Test node

--- Node/test/domain/products/service.test.js
+++ Node/test/domain/products/service.test.js
@@ -2,26 +2,79 @@ const Product = require('../../../src/domain/products/model');
 const productServiceFactory = require('../../../src/domain/products/service');
 
 function createProducts() {
-  const p1 = new Product({ name: 'test1', description: 'Monitor', _id: '1' });
-  const p2 = new Product({ name: 'test2', description: 'Altavoz', _id: '2' });
+  const p1 = new Product({name: 'test1', description: 'Monitor', _id: '1'});
+  const p2 = new Product({name: 'test2', description: 'Altavoz', _id: '2'});
   return [p1, p2];
 }
 
-test('product service test', () => {
-  const findAll = jest.fn();
-  findAll.mockReturnValue(createProducts());
-  let productRepository = { findAll };
-  let productService = productServiceFactory.init({productRepository});
+describe('Product unit tests', () => {
 
-  productService.findAllProducts().then((products) => {
-    expect(products).to.have.lengthOf(2);
-    expect(products).to.eql(createProducts());
-    return done();
+  test('When get all products Then OK', () => {
+    const findAll = jest.fn();
+    findAll.mockReturnValue(createProducts());
+    let productRepository = {findAll};
+    let productService = productServiceFactory.init({productRepository});
+
+    productService.findAllProducts().then((products) => {
+      expect(products.length).toBe(2);
+      expect(products).toStrictEqual(createProducts());
+    });
+
+  });
+
+  test('Given product When create product Then OK', () => {
+    const productToCreate = createProducts()[0];
+    const productCreated = createProducts()[0];
+
+    const createProduct = jest.fn();
+    createProduct.mockReturnValue(productCreated);
+    let productRepository = {createProduct};
+    let productService = productServiceFactory.init({productRepository});
+
+    productService.create(productToCreate)
+    .then((product) => {
+      expect(product.name).toStrictEqual(productToCreate.name);
+      expect(product.description).toStrictEqual(productToCreate.description)
+      expect(product._id).not.toBeNull();
+    });
+
+  });
+
+  test('Given product id When delete Then OK', () => {
+    const id = 1;
+    const productDeleted = createProducts()[0];
+
+    const deleteById = jest.fn();
+    deleteById.mockReturnValue(productDeleted);
+    let productRepository = {deleteById};
+    let productService = productServiceFactory.init({productRepository});
+
+    productService.deleteById(id)
+    .then((product) => {
+      expect(product.name).toStrictEqual(productDeleted.name);
+      expect(product.description).toStrictEqual(productDeleted.description)
+      expect(product.id).not.toBeNull();
+    });
+
+  });
+
+  test('Given id When get product Then OK', () => {
+    const id = 1;
+    const productToGet = createProducts()[0];
+
+    const getProduct = jest.fn();
+    getProduct.mockReturnValue(productToGet);
+    let productRepository = {getProduct};
+    let productService = productServiceFactory.init({productRepository});
+
+    productService.findById(id)
+    .then((product) => {
+      expect(product.name).toStrictEqual(productToGet.name);
+      expect(product.description).toStrictEqual(productToGet.description);
+      expect(product.id).not.toBeNull();
+
+    });
+
   });
 
 });
-
-
-
-
-
---
+++ Node/test/domain/shoppingCarts/service.test.js
@@ -0,0 +1,52 @@
+const ShoppingCart = require('../../../src/domain/shoppingCarts/model');
+const Product = require('../../../src/domain/products/model');
+
+const shoppingCartServiceFactory = require('../../../src/domain/shoppingCarts/service');
+
+function createShoppingCart() {
+  return new ShoppingCart({products: createProducts(), completed: false});
+}
+
+function createEmptyShoppingCart() {
+  return new ShoppingCart({products: [], completed: false});
+}
+
+function createProducts() {
+  const p1 = new Product({name: 'test1', description: 'Monitor', _id: '1'});
+  return [p1];
+}
+
+describe('Shopping cart unit tests', () => {
+
+  test('Given shopping cart When create Then OK', () => {
+    const createShoppingCart = jest.fn();
+    createShoppingCart.mockReturnValue(createEmptyShoppingCart());
+    let shoppingCartRepository = {createShoppingCart};
+    let shoppingCartService = shoppingCartServiceFactory.init({shoppingCartRepository});
+
+    shoppingCartService.create().then((shoppingCart) => {
+      expect(shoppingCart.products.length).toBe(0);
+      expect(shoppingCart.completed).toBeFalsy();
+    });
+
+  });
+
+  test('Given shopping cart, product and quantity When add product Then OK', () => {
+    let shoppingCartToUpdate = createShoppingCart();
+    let productToAdd = createProducts()[0];
+    let quantity = 1;
+
+    const updateShoppingCart = jest.fn();
+    updateShoppingCart.mockReturnValue(shoppingCartToUpdate);
+    let shoppingCartRepository = {updateShoppingCart};
+    let shoppingCartService = shoppingCartServiceFactory.init({shoppingCartRepository});
+
+    shoppingCartService.addProductWithQuantity({shoppingCartToUpdate, productToAdd, quantity}).then((shoppingCart) => {
+      expect(shoppingCart.products.length).toBe(1);
+    });
+
+  });
+
+  // TODO: Test other functions
+  
+});
